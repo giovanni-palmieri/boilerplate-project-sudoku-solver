55b13909edd3f08fc2f100c3291da59404a142dc
giovanni_palmieri00 <giovanni.palmieri@apuliasoft.com>
2026-01-07 20:23:09 +0100

    Add validation logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,27 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (!puzzleString) {
+      return "Required field missing";
+    }
+
+    if (!/^([0-9]|\.)+$/.test(puzzleString)) {
+      return "Invalid characters in puzzle";
+    }
+
+    if (puzzleString.length !== 81) {
+      return "Expected puzzle to be 81 characters long";
+    }
+
+    return null;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,24 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    res.json({});
+  });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route("/api/solve").post((req, res) => {
+    const { puzzle } = req.body;
 
-    });
+    const error = solver.validate(puzzle);
+
+    if (!error) {
+      //const solution = solver.solve(puzzle)
+      res.json({ solution: "" });
+    } else {
+      res.json({ error });
+    }
+  });
 };
