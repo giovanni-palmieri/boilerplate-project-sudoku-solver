d61ded4877640b4b89304f7ce2353ab4f26f69a9
giovanni_palmieri00 <giovanni.palmieri@apuliasoft.com>
2026-01-17 15:03:17 +0100

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -69,4 +69,29 @@ suite("Unit Tests", () => {
     const solver = new Solver(puzzleString);
     assert.isFalse(solver.checkRegionPlacement(solver.grid, 0, 0, "1"));
   });
+
+  test("Valid puzzle strings pass the solver", () => {
+    const input =
+      "5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3";
+    const solution =
+      "568913724342687519197254386685479231219538467734162895926345178473891652851726943";
+    const solver = new Solver(input);
+    assert.equal(solver.solve(), solution);
+  });
+
+  test("Invalid puzzle strings fail the solver", () => {
+    const input =
+      "115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+    const solver = new Solver(input);
+    assert.equal(solver.solve(), false);
+  });
+
+  test("Solver returns the expected solution for an incomplete puzzle", () => {
+    const input =
+      "..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1";
+    const solution =
+      "218396745753284196496157832531672984649831257827549613962415378185763429374928561";
+    const solver = new Solver(input);
+    assert.equal(solver.solve(), solution);
+  });
 });
