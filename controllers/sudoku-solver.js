9c33fb1fb3ec6173ebf84eb365500b1d69708e62
giovanni_palmieri00 <giovanni.palmieri@apuliasoft.com>
2026-01-17 14:30:51 +0100

    Add solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,4 +1,8 @@
 class SudokuSolver {
+  constructor() {
+    this.grid = [];
+  }
+
   validate(puzzleString) {
     if (!puzzleString) {
       return "Required field missing";
@@ -15,13 +19,96 @@ class SudokuSolver {
     return null;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {}
+  checkRowPlacement(row, value) {
+    for (let j = 0; j < 9; j++) {
+      if (this.grid[row][j] == value) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  checkColPlacement(puzzleString, row, column, value) {}
+  checkColPlacement(column, value) {
+    for (let i = 0; i < 9; i++) {
+      if (this.grid[i][column] == value) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  checkRegionPlacement(puzzleString, row, column, value) {}
+  checkRegionPlacement(row, column, value) {
+    const rowIndex = row - (row % 3);
+    const columnIndex = column - (column % 3);
 
-  solve(puzzleString) {}
+    for (let i = rowIndex; i < rowIndex + 3; i++) {
+      for (let j = columnIndex; j < columnIndex + 3; j++) {
+        if (this.grid[i][j] == value) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  solve(puzzleString) {
+    const validationError = this.validate(puzzleString);
+    if (validationError) {
+      return false;
+    }
+
+    this.grid = [];
+    for (let i = 0; i < 81; i += 9) {
+      this.grid.push(puzzleString.slice(i, i + 9).split(""));
+    }
+
+    const solveSudoku = () => {
+      let row = -1;
+      let col = -1;
+      let isEmpty = false;
+      for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          if (this.grid[i][j] === ".") {
+            row = i;
+            col = j;
+            isEmpty = true;
+            break;
+          }
+        }
+        if (isEmpty) {
+          break;
+        }
+      }
+
+      if (!isEmpty) {
+        return true;
+      }
+
+      for (let num = 1; num <= 9; num++) {
+        const value = String(num);
+        const isSafeToPlace =
+          this.checkRowPlacement(row, value) &&
+          this.checkColPlacement(col, value) &&
+          this.checkRegionPlacement(row, col, value);
+
+        if (isSafeToPlace) {
+          this.grid[row][col] = value;
+          if (solveSudoku()) {
+            return true;
+          }
+          this.grid[row][col] = ".";
+        }
+      }
+
+      return false;
+    };
+
+    if (solveSudoku()) {
+      return this.grid.map((row) => row.join("")).join("");
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = SudokuSolver;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,10 +2,11 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Solver = require("../controllers/sudoku-solver.js");
-let solver = new Solver();
 
 suite("Unit Tests", () => {
   test("Logic handles a valid puzzle string of 81 characters", () => {
+    let solver = new Solver();
+
     const input =
       "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 
